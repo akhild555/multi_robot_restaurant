cf190a4c01ad398e466d7e015c5fe4d160ed3b6f
Akhil Devarakonda <adevara5@seas.upenn.com>
2020-10-27 14:13:15 -0700

    update task_allocator CMakeLists

--- catkin_ws/build/catkin_generated/setup_cached.sh
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -8,7 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/akhil/mr_rest/multi_robot_restaurant/catkin_ws/devel:$CMAKE_PREFIX_PATH"
-export PATH='/opt/ros/melodic/bin:/home/akhil/bin:/home/akhil/.local/bin:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin'
+export LD_LIBRARY_PATH="/home/akhil/mr_rest/multi_robot_restaurant/catkin_ws/devel/lib:$LD_LIBRARY_PATH"
+export PKG_CONFIG_PATH="/home/akhil/mr_rest/multi_robot_restaurant/catkin_ws/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD='/home/akhil/mr_rest/multi_robot_restaurant/catkin_ws/build'
 export ROSLISP_PACKAGE_DIRECTORIES='/home/akhil/mr_rest/multi_robot_restaurant/catkin_ws/devel/share/common-lisp'
 export ROS_PACKAGE_PATH="/home/akhil/mr_rest/multi_robot_restaurant/catkin_ws/src:$ROS_PACKAGE_PATH"
--- catkin_ws/build/task_allocator/catkin_generated/pkg.installspace.context.pc.py
+++ catkin_ws/build/task_allocator/catkin_generated/pkg.installspace.context.pc.py
@@ -1,7 +1,7 @@
 # generated from catkin/cmake/template/pkg.context.pc.in
 CATKIN_PACKAGE_PREFIX = ""
 PROJECT_PKG_CONFIG_INCLUDE_DIRS = "".split(';') if "" != "" else []
-PROJECT_CATKIN_DEPENDS = "".replace(';', ' ')
+PROJECT_CATKIN_DEPENDS = "roscpp;rospy;std_msgs".replace(';', ' ')
 PKG_CONFIG_LIBRARIES_WITH_PREFIX = "".split(';') if "" != "" else []
 PROJECT_NAME = "task_allocator"
 PROJECT_SPACE_DIR = "/home/akhil/mr_rest/multi_robot_restaurant/catkin_ws/install"

797fb70093bbfa6a7ba17d5e10307caf7097a633
varunvelpula <vvvarun1@gmail.com>
2020-11-12 12:10:55 -0800

    added script to run nodes in parallel

--- run_all.sh
+++ run_all.sh
@@ -4,9 +4,9 @@ rosbuild_ws/simulator/ut_multirobot_sim/bin/simulator --sim_config=rosbuild_ws/s
 cd catkin_ws
 devel/lib/control_stack/nav_node src/control_stack/config/sim_config0.lua 0 &
 cd catkin_ws
-devel/lib/control_stack/nav_node --src/control_stack/config/sim_config1.lua 1 &
+devel/lib/control_stack/nav_node src/control_stack/config/sim_config1.lua 1 &
 cd catkin_ws
-devel/lib/control_stack/nav_node --src/control_stack/config/sim_config2.lua 2 &
+devel/lib/control_stack/nav_node src/control_stack/config/sim_config2.lua 2 &
 # cd catkin_ws
 # devel/lib/control_stack/nav_node --src/control_stack/config/sim_config3.lua 3 &
 # cd catkin_ws
